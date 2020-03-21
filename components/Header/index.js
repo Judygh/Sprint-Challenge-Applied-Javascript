44fa0f751a10e377b74e76a902d91c8a785dbf48
Judy Ghashim <judyghashim@Judys-MacBook-Pro.local>
2020-03-21 11:27:59 -0500

    start working on header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,43 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header(data) {
+  // create element
+  const theHeader = document.createElement("div");
+  const theDate = document.createElement("span");
+  const title = document.createElement("h1");
+  const theTemp = document.createElement("span");
+
+  // add classes
+  theHeader.classList.add("header");
+  theDate.classList.add("date");
+  theTemp.classList.add("temp");
+
+  // add content
+  theDate.textContent = span;
+  title.textContent = h1;
+  theHeader.textContent = span;
+
+  //append
+  theHeader.append(theDate);
+  theHeader.append(title);
+  theHeader.append(theTemp);
+
+  return theHeader;
+}
+const parentTag = document.querySelector(".header-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    // deal with the response data in here
+
+    const headerData = response.data.topics;
+    parentTag.append(Header(headerData));
+
+    // console.log(response.data);
+  })
+  .catch(err => {
+    // deal with the error in here
+    console.log("CATCH: the data was not returned", err);
+  });
