89f47e93a6bb1727d65a6b766962268ada9050c2
Judy Ghashim <judyghashim@Judys-MacBook-Pro.local>
2020-03-21 12:54:29 -0500

    working on taps

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,45 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// const Tap = data => {
+//   const tabs = document.createElement("div");
+//   tabs.textContent = data;
+//   tabs.classList.add("tab");
+//   return tabs;
+// };
+// // console.log("structure");
+
+function newTabs(BANANA) {
+  // create element
+  console.log(` BANANA: `, BANANA);
+  const tabOne = document.createElement("div");
+
+  //add class
+  tabOne.classList.add("tab");
+
+  //add content
+  tabOne.textContent = BANANA;
+  console.log(`THIS IS TAG CONTENT: `, tabOne.textContent);
+
+  return tabOne;
+}
+
+const parentTab = document.querySelector(".topics");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    const entries = Object.values(response.data.topics);
+
+    entries.forEach(item => {
+      parentTab.append(newTabs(item));
+    });
+
+    // console.log(`this is my forEACHHH: `, myforEach);
+
+    // console.log("THIS IS TABS CONSOLE", response.data.topics);
+  })
+  .catch(err => {
+    console.log("the data was not returned on the TABSSSS", err);
+  });
